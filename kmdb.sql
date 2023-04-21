1ba73ae029d68283475efe7f714a62d29b10d0b1
myoung kyu lee <myoungkyu.lee@kellogg.northwestern.edu>
2023-04-21 02:16:34 +0000

    final

--- kmdb.sql
+++ kmdb.sql
@@ -105,13 +105,84 @@
 
 -- Drop existing tables, so you'll start fresh each time this script is run.
 -- TODO!
+DROP TABLE IF EXISTS movies;
+DROP TABLE IF EXISTS studios;
+DROP TABLE IF EXISTS actors;
+DROP TABLE IF EXISTS players;
 
 -- Create new tables, according to your domain model
 -- TODO!
+CREATE TABLE movies (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    title TEXT,
+    year_released INTEGER,
+    rating_mdaa TEXT,
+    studio_id INTEGER
+);
+
+CREATE TABLE studios (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    company TEXT
+);
+
+CREATE TABLE actors (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name TEXT
+);
+
+CREATE TABLE players (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    character TEXT,
+    actor_id INTEGER,
+    movie_id INTEGER
+);
 
 -- Insert data into your database that reflects the sample data shown above
 -- Use hard-coded foreign key IDs when necessary
 -- TODO!
+INSERT INTO movies (
+    title,
+    year_released,
+    rating_mdaa,
+    studio_id
+)
+VALUES
+("Batman Begins", 2005, "PG-13", 1),
+("The Dark Knight", 2008, "PG-13",1),
+("The Dark Knight Rises", 2012, "PG-13", 1);
+
+INSERT INTO studios (company)
+VALUES ("Warner Bros.");
+
+INSERT INTO actors (name)
+VALUES ("Christian Bale"), 
+    ("Michael Caine"), 
+    ("Liam Neeson"), 
+    ("Katie Holmes"), 
+    ("Gary Oldman"), 
+    ("Heath Ledger"), 
+    ("Aaron Eckhart"),
+    ("Maggie Gyllenhaal"), 
+    ("Tom Hardy"),
+    ("Joseph Gordon-Levitt"),
+    ("Anne Hathaway");
+
+INSERT INTO players (character, movie_id, actor_id)
+VALUES ("Bruce Wayne", 1, 1), 
+    ("Alfred", 1, 2), 
+    ("Ra's Al Ghul", 1, 3), 
+    ("Rachel Dawes", 1, 4), 
+    ("Commissioner Gordon", 1, 5), 
+    ("Bruce Wayne", 2, 1), 
+    ("Joker", 2, 6),
+    ("Harvey Dent", 2, 7), 
+    ("Alfred", 2, 2),
+    ("Rachel Dawes", 2, 8),
+    ("Bruce Wayne", 3, 1),
+    ("Commissioner Gordon", 3, 5),
+    ("Bane", 3, 9), 
+    ("John Blake", 3, 10),
+    ("Selina Kyle", 3, 11);
 
 -- Prints a header for the movies output
 .print "Movies"
@@ -120,6 +191,8 @@
 
 -- The SQL statement for the movies output
 -- TODO!
+SELECT movies.title, movies.year_released, movies.rating_mdaa, studios.company 
+FROM movies INNER JOIN studios ON movies.studio_id = studios.id;
 
 -- Prints a header for the cast output
 .print ""
@@ -130,3 +203,6 @@
 
 -- The SQL statement for the cast output
 -- TODO!
+SELECT movies.title, actors.name, players.character 
+FROM players INNER JOIN movies ON players.movie_id = movies.id
+             INNER JOIN actors ON players.actor_id = actors.id;
